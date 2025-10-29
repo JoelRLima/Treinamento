d8903faa37b02edaec1754a294c9f5176f6e8442
Joel Lima <116195128+JoelRLima@users.noreply.github.com>
2025-10-29 13:34:54 -0300

    Update calculadora.py

--- Calculadora_Python/calculadora.py
+++ Calculadora_Python/calculadora.py
@@ -1,15 +1,63 @@
-operadores = ['*', '/','+','-']
 
+#------------------------
+#   Pré-Processamento
+#------------------------
+operadores = ['*', '/','+','-']
 roberto = True
+partial_numbers = []
+operators_sequence = []
+sentence = input("Digite a expressão entre dois numeros: ")
 while(roberto):
-    sentence = input("Digite a expressão entre dois numeros: ")
     PosOps = 0
     for i, letter in enumerate(sentence):
         if (letter in operadores):
             a = int(sentence[PosOps:i])
+            partial_numbers.append(a)
             PosOps = i+1
-            print(f"achei o operador {letter} na posicão {i}")
-            print(f"Oq veio antes do Operador: {a}")
+            operators_sequence.append(letter)
+            #print(f"achei o operador {letter} na posicão {i}")
+            #print(f"Oq veio antes do Operador: {a}")
     ultimo = int(sentence[PosOps:])
-    print(f"Ultimo numero encontrado: {ultimo}")
+    partial_numbers.append(ultimo)
+    #print(f"Ultimo numero encontrado: {ultimo}")
     roberto = False
+print(partial_numbers)
+print(operators_sequence)
+
+#------------------------
+#      Processamento
+#------------------------
+
+
+while(len(operators_sequence) != 0):
+    if '*' in operators_sequence:
+        operators_index = operators_sequence.index('*')
+        resultado = partial_numbers[operators_index] * partial_numbers[operators_index+1]
+        partial_numbers.pop(operators_index)
+        partial_numbers.pop(operators_index)
+        partial_numbers.insert(operators_index, resultado)
+        operators_sequence.remove('*')
+    elif '+' in operators_sequence:
+        operators_index = operators_sequence.index('+')
+        resultado = partial_numbers[operators_index] + partial_numbers[operators_index+1]
+        partial_numbers.pop(operators_index)
+        partial_numbers.pop(operators_index)
+        partial_numbers.insert(operators_index, resultado)
+        operators_sequence.remove('+')
+    elif '-' in operators_sequence:
+        operators_index = operators_sequence.index('-')
+        resultado = partial_numbers[operators_index] - partial_numbers[operators_index+1]
+        partial_numbers.pop(operators_index)
+        partial_numbers.pop(operators_index)
+        partial_numbers.insert(operators_index, resultado)
+        operators_sequence.remove('-')
+
+print(partial_numbers)
+print(operators_sequence)
+
+# [12, 56, 42, 43]
+# ['+', '*', '+']
+# 3
+
+12+56*42+43*6-70
+
