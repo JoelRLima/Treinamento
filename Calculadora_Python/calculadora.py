518090f06e4dc79c223edbb24bd906b351567b26
Joel Lima <116195128+JoelRLima@users.noreply.github.com>
2025-10-29 14:21:24 -0300

    Update calculadora.py
    add classe POO

--- Calculadora_Python/calculadora.py
+++ Calculadora_Python/calculadora.py
@@ -1,65 +1,69 @@
-#------------------------
-#   Pré-Processamento
-#------------------------
-operadores = ['*', '/','+','-']
-roberto = True
-partial_numbers = []
-operators_sequence = []
-sentence = input("Digite a expressão entre dois numeros: ")
-while(roberto):
-    PosOps = 0
-    for i, letter in enumerate(sentence):
-        if (letter in operadores):
-            a = int(sentence[PosOps:i])
-            partial_numbers.append(a)
-            PosOps = i+1
-            operators_sequence.append(letter)
-            #print(f"achei o operador {letter} na posicão {i}")
-            #print(f"Oq veio antes do Operador: {a}")
-    ultimo = int(sentence[PosOps:])
-    partial_numbers.append(ultimo)
-    #print(f"Ultimo numero encontrado: {ultimo}")
-    roberto = False
-print(partial_numbers)
-print(operators_sequence)
+class Calculadora:
+    def __init__(self):
+        self.partial_numbers = list()
+        self.operators_sequence = list()
+        self.resultado = 0
 
-#------------------------
-#      Processamento
-#------------------------
+    def calcular(self, sentence):
+        self._pre_processamento(sentence)
+        self._processamento()
 
-while(len(operators_sequence) != 0):
-    if '*' in operators_sequence or '/' in operators_sequence:
-        if '*' in operators_sequence:
-            operators_index = operators_sequence.index('*')
-            resultado = partial_numbers[operators_index] * partial_numbers[operators_index+1]
-            partial_numbers.pop(operators_index)
-            partial_numbers.pop(operators_index)
-            partial_numbers.insert(operators_index, resultado)
-            operators_sequence.remove('*')
-        else:
-            operators_index = operators_sequence.index('/')
-            resultado = partial_numbers[operators_index] / partial_numbers[operators_index+1]
-            partial_numbers.pop(operators_index)
-            partial_numbers.pop(operators_index)
-            partial_numbers.insert(operators_index, resultado)
-            operators_sequence.remove('/')
-    elif '+' in operators_sequence:
-        operators_index = operators_sequence.index('+')
-        resultado = partial_numbers[operators_index] + partial_numbers[operators_index+1]
-        partial_numbers.pop(operators_index)
-        partial_numbers.pop(operators_index)
-        partial_numbers.insert(operators_index, resultado)
-        operators_sequence.remove('+')
-    elif '-' in operators_sequence:
-        operators_index = operators_sequence.index('-')
-        resultado = partial_numbers[operators_index] - partial_numbers[operators_index+1]
-        partial_numbers.pop(operators_index)
-        partial_numbers.pop(operators_index)
-        partial_numbers.insert(operators_index, resultado)
-        operators_sequence.remove('-')
+    def _pre_processamento(self, sentence):
+        """
+            Pré-Processamento
+        """
+        operadores = ['*', '/','+','-']
+        roberto = True
+        self.partial_numbers = []
+        self.operators_sequence = []
+        while(roberto):
+            PosOps = 0
+            for i, letter in enumerate(sentence):
+                if (letter in operadores):
+                    a = int(sentence[PosOps:i])
+                    self.partial_numbers.append(a)
+                    PosOps = i+1
+                    self.operators_sequence.append(letter)
+                    #print(f"achei o operador {letter} na posicão {i}")
+                    #print(f"Oq veio antes do Operador: {a}")
+            ultimo = int(sentence[PosOps:])
+            self.partial_numbers.append(ultimo)
+            #print(f"Ultimo numero encontrado: {ultimo}")
+            roberto = False
+        print(self.partial_numbers)
+        print(self.operators_sequence)
 
-print(partial_numbers)
-print(operators_sequence)
+    def _calculo_basico(self, operador):
+        operators_index = self.operators_sequence.index(operador)
+        if operador == '*':
+            resultado = self.partial_numbers[operators_index] * self.partial_numbers[operators_index+1]
+        elif operador == '/':
+            resultado = self.partial_numbers[operators_index] / self.partial_numbers[operators_index+1]
+        elif operador == '-':
+            resultado = self.partial_numbers[operators_index] - self.partial_numbers[operators_index+1]
+        elif operador == '+':
+            resultado = self.partial_numbers[operators_index] + self.partial_numbers[operators_index+1]
+        self.partial_numbers.pop(operators_index)
+        self.partial_numbers.pop(operators_index)
+        self.partial_numbers.insert(operators_index, resultado)
+        self.operators_sequence.remove(operador)
+
+    def _processamento(self):
+        while(len(self.operators_sequence) != 0):
+            if '*' in self.operators_sequence or '/' in self.operators_sequence:
+                if '/' in self.operators_sequence:
+                    self._calculo_basico('/')
+                else:
+                    self._calculo_basico('*')
+            elif '+' in self.operators_sequence:
+                self._calculo_basico('+')
+            elif '-' in self.operators_sequence:
+                self._calculo_basico('-')
+        self.resultado = self.partial_numbers[0]
+
+Calc = Calculadora()
+Calc.calcular("12+56*42+43/6-70")
+print(Calc.resultado)
 
 # [12, 56, 42, 43]
 # ['+', '*', '+']
